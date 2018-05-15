273406db65a8029fcdf149399fa0e413c59f56a7
r1-21 <r1-21@JeongKS>
2018-05-15 10:41:30 +0900

    05-15

---
+++ 뇌를 자극하는 자바/src/chap09/After100Days.java
@@ -0,0 +1,10 @@
+package chap09;
+
+public class After100Days {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- 뇌를 자극하는 자바/src/chap09/StringExample1.java
+++ 뇌를 자극하는 자바/src/chap09/StringExample1.java
@@ -14,12 +14,17 @@ public class StringExample1 {
 
 		String str1 = new String("Hello, Java");
 		String str2 = new String("Hello, Java");
-		String str3 = new String("Hello, Java");
+		// String str3 = new String("Hello, Java");
+		// String str1 = "Hello, Java";
+		// String str2 = "Hello, Java";
 
-		System.out.println(str1);
-		System.out.println(str2);
-		System.out.println(str3);
+		// System.out.println(str1);
+		// System.out.println(str2);
 
+		if (str1.equals(str2))
+			System.out.println("같다");
+		else
+			System.out.println("다르다");
 	}
 
 }
