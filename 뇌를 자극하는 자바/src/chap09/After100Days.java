efd24066dafc089081ac7dbd28dbe4ec9d00b409
r1-21 <r1-21@JeongKS>
2018-05-15 17:57:09 +0900

    0515

--- 뇌를 자극하는 자바/src/chap09/After100Days.java
+++ 뇌를 자극하는 자바/src/chap09/After100Days.java
@@ -1,9 +1,21 @@
 package chap09;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 public class After100Days {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());// 현재시간 설정
+		calendar.add(Calendar.DATE, 100);
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy년 MM월 dd일");
+		String date = dateFormat.format(calendar.getTime());
+		System.out.println(date);
 
 	}
 
