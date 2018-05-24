1ca239c206687e93fd0542b33a4c3773f692afd8
r1-21 <r1-21@JeongKS>
2018-05-24 12:22:46 +0900

    05-24

--- 뇌를 자극하는 자바/src/chap19/WindowExample2.java
+++ 뇌를 자극하는 자바/src/chap19/WindowExample2.java
@@ -4,14 +4,13 @@ import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionListener;
-
-//import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-//import javax.swing.JPanel;
 import javax.swing.JTextField;
 //import javax.swing.SwingConstants;
+//import javax.swing.ImageIcon;
+//import javax.swing.JPanel;
 
 public class WindowExample2 {
 
